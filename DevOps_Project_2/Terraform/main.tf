b3493ae001481f95930a8ebe80ea5d0f6d8e22cb
pawaranurag123 <95554131+pawaranurag123@users.noreply.github.com>
2023-02-13 15:01:30 +0530

    Update main.tf

--- DevOps_Project_2/Terraform/main.tf
+++ DevOps_Project_2/Terraform/main.tf
@@ -16,7 +16,7 @@ data "aws_ami" "amazon-linux" {
 resource "aws_instance" "dev_machine" {
   ami = data.aws_ami.amazon-linux.id
   instance_type = "t2.micro"
-  key_name = "jenkins"
+  key_name = "jenkins1"
   
   tags = {
     Environment = "dev"
