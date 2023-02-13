f4e2f1ef99fcf4b13f03e2a91a69c7c6e053368a
pawaranurag123 <95554131+pawaranurag123@users.noreply.github.com>
2023-02-13 10:17:21 +0530

    Update main.tf

--- DevOps_Project_2/Terraform/main.tf
+++ DevOps_Project_2/Terraform/main.tf
@@ -17,9 +17,7 @@ resource "aws_instance" "dev_machine" {
   ami = data.aws_ami.amazon-linux.id
   instance_type = "t2.micro"
   key_name = "jenkins"
-  vpc_security_group_ids = ["sg-0649ecfc12248c271"]
-  subnet_id              = "subnet-0e6a5ed6484656643"
-
+  
   tags = {
     Environment = "dev"
     Name = "${var.name}-server"
