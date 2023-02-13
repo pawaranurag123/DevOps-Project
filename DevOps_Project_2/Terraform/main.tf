249e4e1881c1bcb5319141e568bf80574f21d7ef
pawaranurag123 <95554131+pawaranurag123@users.noreply.github.com>
2023-02-13 10:02:27 +0530

    Update main.tf

--- DevOps_Project_2/Terraform/main.tf
+++ DevOps_Project_2/Terraform/main.tf
@@ -11,28 +11,12 @@ data "aws_ami" "amazon-linux" {
     values = ["hvm"]
   }
 }
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.16.0.0/16"
 
-  tags = {
-    Name = "tf-example"
-  }
-}
-
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-west-2a"
-
-  tags = {
-    Name = "tf-example"
-  }
-}
 
 resource "aws_instance" "dev_machine" {
   ami = data.aws_ami.amazon-linux.id
   instance_type = "t2.micro"
-  key_name = "euran-jenkins"
+  key_name = "jenkins"
 
   tags = {
     Environment = "dev"
